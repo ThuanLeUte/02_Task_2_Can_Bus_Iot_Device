09489b6c1e404a01816483da854e59a06e68c302
ThuanLe <58209201+ThuanLeUte@users.noreply.github.com>
2021-02-24 18:17:48 +0700

    Update as5048

--- Core/Src/as5048a.h
+++ Core/Src/as5048a.h
@@ -33,8 +33,10 @@ as5048a_status_t;
 /**
  * @brief AS5048A structure
  */
-typedef struct 
+typedef struct
 {
+  uint16_t position;
+
   int (*spi_read)  (uint8_t *p_data, uint16_t len, uint32_t time_out);
   int (*spi_write) (uint8_t *p_data, uint16_t len, uint32_t time_out);
 }
@@ -44,19 +46,67 @@ as5048a_t;
 /* Public variables --------------------------------------------------- */
 /* Public function prototypes ----------------------------------------- */
 /**
- * @brief  <function description>
+ * @brief         AS5024A get rotation in degrees
  *
- * @param[in]     <param_name>  <param_despcription>
- * @param[out]    <param_name>  <param_despcription>
- * @param[inout]  <param_name>  <param_despcription>
+ * @param[in]     me        Pointer to as5048a structure
+ * @param[in]     degrees   Pointer to degrees
+ * 
+ * @attention     None
  *
- * @attention  <API attention note>
+ * @return        AS5048A_OK
+ * @return        AS5048A_ERR_SPI
+ */
+as5048a_status_t as5048a_get_rotation_in_degrees(as5048a_t *me, float *degrees);
+
+/**
+ * @brief         AS5024A get rotation
  *
- * @return  
- *  - 0: Success
- *  - 1: Error
+ * @param[in]     me        Pointer to as5048a structure
+ * @param[in]     rotation  Pointer to rotation
+ * 
+ * @attention     None
+ *
+ * @return        AS5048A_OK
+ * @return        AS5048A_ERR_SPI
+ */
+as5048a_status_t as5048a_get_rotation(as5048a_t *me, uint16_t *rotation);
+
+/**
+ * @brief         AS5024A get raw rotation
+ *
+ * @param[in]     me        Pointer to as5048a structure
+ * @param[in]     rotation  Pointer to rotation
+ * 
+ * @attention     None
+ *
+ * @return        AS5048A_OK
+ * @return        AS5048A_ERR_SPI
  */
 as5048a_status_t as5048a_get_raw_rotation(as5048a_t *me, uint16_t *rotation);
+
+/**
+ * @brief         AS5024A set zero position
+ *
+ * @param[in]     me        Pointer to as5048a structure
+ * @param[in]     position  Position
+ * 
+ * @attention     None
+ *
+ * @return        None
+ */
+void as5048a_set_zero_position(as5048a_t *me, uint16_t position);
+
+/**
+ * @brief         AS5024A get zero postion
+ *
+ * @param[in]     me        Pointer to as5048a structure
+ * @param[in]     position  Pointer to position
+ * 
+ * @attention     None
+ *
+ * @return        None
+ */
+void as5048a_get_zero_position(as5048a_t *me, uint16_t *position);
 
 #endif // __AS5048A_H
 
